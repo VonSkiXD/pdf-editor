431d5bc1667c3a09009faa275da9df1f4fd19f4f
VonSkiXD <879818327@qq.com>
2022-08-15 15:12:45 +0800

    feat:new pdt test

---
+++ src/pages/pdf-lib-test/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
+
+async function createPdf() {
+  const pdfDoc = await PDFDocument.create()
+  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+
+  const page = pdfDoc.addPage()
+  const { width, height } = page.getSize()
+  const fontSize = 30
+  page.drawText('Creating PDFs in JavaScript is awesome!', {
+    x: 50,
+    y: height - 4 * fontSize,
+    size: fontSize,
+    font: timesRomanFont,
+    color: rgb(0, 0.53, 0.71),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+}
+</script>
+<template>
+  <div>
+  ceshi
+    </div>
+</template>
+<route lang="yaml">
+meta:
+  layout: default
+</route>
