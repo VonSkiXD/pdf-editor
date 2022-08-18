9a15c8f61c7f86b50a4d5b4bfebc2ba5bfe5c639
fengjun <879818327@qq.com>
2022-08-18 11:37:23 +0800

    make pdf show in home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,28 +2,31 @@
 import * as pdfjsLib from 'pdfjs-dist'
 import * as pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry'
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker
-const p = ref()
-console.log(p)
-const loadingTask = pdfjsLib.getDocument('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf').promise
-const pdf = await loadingTask
 
-const firstPageNumber = 1
-console.log(pdf)
-const page = await pdf.getPage(firstPageNumber)
-console.log(page)
-const scale = 1.5
-const viewport = page.getViewport({ scale })
-console.log(viewport)
-// const p = document.getElementById('p1111')
+const canItem = ref()
 
-const renderContext = {
-  canvasContext: p,
-  viewport,
+async function renderPdf() {
+  const loadingTask = pdfjsLib.getDocument('https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf').promise
+  const pdf = await loadingTask
+  const firstPageNumber = 1
+  const page = await pdf.getPage(firstPageNumber)
+  const scale = 1.5
+  const viewport = page.getViewport({ scale })
+
+  const canvasC = canItem.value.getContext('2d')
+
+  const renderContext = {
+    canvasContext: canvasC,
+    viewport,
+  }
+
+  const renderTask = page.render(renderContext)
+  await renderTask.promise
 }
-
-const renderTask = page.render(renderContext)
-
-await renderTask
+onMounted(() => {
+  renderPdf()
+})
+// await renderTask
 </script>
 
 <template>
@@ -31,11 +34,7 @@ await renderTask
     <!-- <button @click="createPdf()">
       下载
     </button> -->
-    <canvas
-      id="p1111"
-      width="200"
-      height="100"
-    />
+    <canvas ref="canItem" width="1920" height="1080" />
     <img src="/favicon.svg" alt="1">
   </div>
 </template>
